f3b4eb5dc42b7555f072d58dfdc9567012da256c
patrickcuic <111729668+PattyMakes@users.noreply.github.com>
2023-03-28 17:20:02 +0200

    Update

--- uin23ak4_moviesearch_cuic/src/App.js
+++ uin23ak4_moviesearch_cuic/src/App.js
@@ -1,42 +1,32 @@
 //KILDER: https://www.freecodecamp.org/news/react-movie-app-tutorial/
 
-import { useState } from 'react';
 import './css/main.css';
 import SearchResults from './components/SearchResults';
+import { useState } from 'react';
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('');
-  const [movies, setMovies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('james+bond');
 
-  const handleSearchChange = (event) => {
-    setSearchQuery(event.target.value);
-  };
-
-  const searchMovies = async () => {
-    if (searchQuery.length < 3) {
-      return;
-    }
-  
-    try {
-      const response = await fetch(`https://www.omdbapi.com/?apikey=7138b361&s=${searchQuery}&type=movie&plot=full`);
-      const data = await response.json();
-      setMovies(data.Search);
-    } catch (error) {
-      console.error(error);
-    }
+  const handleSearch = query => {
+    setSearchQuery(query || 'james+bond');
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Search</h1>
-        <div className="search-container">
-          <input type="text" placeholder="Search for a movie" value={searchQuery} onChange={handleSearchChange} />
-          <button onClick={searchMovies}>Search</button>
-        </div>
       </header>
       <main>
-        {searchQuery.length >= 3 && movies && <SearchResults movies={movies} />}
+        <div>
+          <input
+            type="text"
+            name="query"
+            placeholder="Search for movies..."
+            onChange={e => handleSearch(e.target.value)}
+          />
+          <button onClick={() => handleSearch(searchQuery)}>Search</button>
+        </div>
+        <SearchResults searchQuery={searchQuery} />
       </main>
     </div>
   );
--- uin23ak4_moviesearch_cuic/src/components/SearchResults.js
+++ uin23ak4_moviesearch_cuic/src/components/SearchResults.js
@@ -1,40 +1,49 @@
 //KILDER: https://legacy.reactjs.org/docs/hooks-effect.html
 //KILDER: https://stackoverflow.com/questions/62341026/movies-map-is-not-a-function
 
-import '../css/main.css';
 import { useState, useEffect } from 'react';
 import { MovieCard } from './MovieCard';
 
-function MovieList() {
+function SearchResults({ searchQuery }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const getMovies = async () => {
-        const response = await fetch('https://www.omdbapi.com/?s=james+bond&apikey=7138b361&type=movie');
+      try {
+        const response = await fetch(`https://www.omdbapi.com/?s=${searchQuery}&apikey=7138b361&type=movie`);
         const data = await response.json();
-        const movieIds = data.Search.map(movie => movie.imdbID);
-        const moviesWithDetails = await Promise.all(movieIds.map(async id => {
-          const response = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=7138b361&plot=full`);
-          const data = await response.json();
-          return data;
-        }));
-        setMovies(moviesWithDetails);
-      };
+        if (data.Search) {
+          const movieIds = data.Search.slice(0, 10).map(movie => movie.imdbID);
+          const moviesWithDetails = await Promise.all(movieIds.map(async id => {
+            const response = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=7138b361&plot=full`);
+            const data = await response.json();
+            return data;
+          }));
+          setMovies(moviesWithDetails);
+        } else {
+          setMovies([]);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
     getMovies();
-  }, []);
+  }, [searchQuery]);
 
   return (
-    <div className="movie-list">
-      {movies.map(movie => (
-        <MovieCard
-          key={movie.imdbID}
-          movie={movie}
-          poster={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/250x250'}
-        />
-      ))}
+    <div>
+      <div className="movie-list">
+        {movies.map(movie => (
+          <MovieCard
+            key={movie.imdbID}
+            movie={movie}
+            poster={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/250x250'}
+          />
+        ))}
+      </div>
     </div>
   );
 }
 
-export default MovieList;
+export default SearchResults;
